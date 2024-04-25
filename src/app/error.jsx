a54b9bdb65c0bdbf2402c6f250f55f1a7afd62ee
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-04-25 09:36:27 -0700

    Adding error page

---
+++ src/app/error.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+function error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again</h1>
+      <buttton className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </buttton>
+    </div>
+  );
+}
+
+export default error;
