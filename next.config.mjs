97bf19eedc69e39baacaed7603e1d1a2a3e517b4
Rodrigo Calles <joserodrigoc98@gmail.com>
2024-04-29 10:46:58 -0700

    fixing config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "**",
+      },
+    ],
   },
+  experimental: { missingSuspenseWithCSRBailout: false },
 };
 
 export default nextConfig;
